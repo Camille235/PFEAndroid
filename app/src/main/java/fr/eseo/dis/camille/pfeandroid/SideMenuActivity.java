eaec3639a4f9c74c2122d7603ffb64781c9b6e74
JeromeGuenet <jerome.guenet@reseau.eseo.fr>
2017-12-20 16:25:21 +0100

    Merge branch 'master' of C:\Users\Jerome\AppData\Local\Android\AndroidStudioProjects\PFEAndroid with conflicts.

---
+++ app/src/main/java/fr/eseo/dis/camille/pfeandroid/SideMenuActivity.java
@@ -0,0 +1,15 @@
+package fr.eseo.dis.camille.pfeandroid.webServiceBean;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.eseo.dis.camille.pfeandroid.R;
+
+public class SideMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_side_menu);
+    }
+}
